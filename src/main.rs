c18d9f6c76b963c9c356e9719ac7941ca6b6d893
SachinMaharana <sachin.nicky@gmail.com>
2021-02-20 03:17:11 +0530

    chore: refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 #[global_allocator]
 static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-use anyhow::Result;
+use anyhow::{bail, format_err, Result};
 use crossbeam_channel as channel;
 use itertools::{concat, Itertools};
 use serde::{Deserialize, Serialize};
 use std::thread;
-use std::time::Duration;
 use std::{
     env, fs,
     path::{Path, PathBuf},
 };
+use std::{str::FromStr, time::Duration};
 use threadpool::ThreadPool;
 // const DEFAULT_CONFIG_PATH = ""
 const DEFAULT_CONFIG_FILE_NAME: &str = "default.toml";
@@ -47,6 +47,12 @@ struct Config {
     subreddit: Vec<String>,
 }
 
+impl FromStr for Config {
+    type Err = toml::de::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        toml::from_str(s)
+    }
+}
 impl Config {
     fn to_file(&self, config_path: &Path) -> Result<()> {
         let toml = toml::to_string(self)?;
@@ -56,17 +62,7 @@ impl Config {
     }
 }
 
-fn main() -> Result<()> {
-    let saved_path = "/home/sachin/.config/polybar/current_post.txt";
-    let config_file = get_global_config_path()?;
-    if !config_file.exists() {
-        println!(
-            "Config File Doesn't exist. Run polybar-reddit init to create one with default values"
-        );
-        std::process::exit(1)
-    }
-    println!("File {:?}", config_file);
-
+fn init(config_file: &Path) -> Result<()> {
     let mut subreddits = Vec::new();
     subreddits.push("politics".to_string());
     subreddits.push("movies".to_string());
@@ -76,11 +72,72 @@ fn main() -> Result<()> {
     };
 
     config.to_file(&config_file)?;
+    Ok(())
+}
+
+fn get_config(config_file: &Path) -> Option<Config> {
+    match fs::read_to_string(&config_file) {
+        Ok(contents) => match Config::from_str(&contents) {
+            Ok(config) => Some(config),
+            Err(_) => None,
+        },
+        Err(_) => None,
+    }
+}
+
+fn main() -> Result<()> {
+    let saved_path = "/home/sachin/.config/polybar/current_post.txt";
+    let config_file = get_global_config_path()?;
+    println!("{:?}", config_file);
+    if let Some(c) = env::args().nth(1) {
+        if c == "init" {
+            init(&config_file).unwrap();
+            println!(
+                "Successfully Created. Edit the file {} and run polybar-reddit",
+                &config_file.display()
+            );
+            std::process::exit(0)
+        }
+    }
+
+    if !config_file.exists() {
+        println!(
+            "Config File Doesn't exist. Run polybar-reddit init to create one with default values"
+        );
+        std::process::exit(1)
+    }
+    let subreddits = match get_config(&config_file) {
+        Some(config) => config.subreddit,
+        None => bail!("Not valid Reddits Found"),
+    };
+
+    if subreddits.is_empty() {
+        bail!("Add comma separated subreddits");
+    }
+    // let subreddits = subreddits.iter().map(|s| {
+    //     if s.is_empty() {
+    //         bail!("Should not be emmpty")
+    //     } else {
+    //         return s;
+    //     }
+    // });
+
+    if subreddits.contains(&"".to_string()) {
+        bail!("No empty string allowed")
+    }
+
+    for s in &subreddits {
+        let url = request_url_builders(s.to_owned());
+        let resp = ureq::get(&url).call();
+        if resp.status() != 200 {
+            bail!("{} subreddits not found", s);
+        }
+    }
 
     let (tx, rx) = channel::unbounded();
     let pool = ThreadPool::new(4);
 
-    for sub in config.subreddit {
+    for sub in subreddits {
         let url = request_url_builder(sub);
         let tx = tx.clone();
 
@@ -106,11 +163,23 @@ fn main() -> Result<()> {
 fn request_url_builder(subreddit: String) -> String {
     format!("https://www.reddit.com/r/{}.json?limit=10", subreddit)
 }
+fn request_url_builders(subreddit: String) -> String {
+    format!("https://www.reddit.com/r/{}", subreddit)
+}
 
 fn make_request(tx: channel::Sender<Vec<ChildrenData>>, url: &str) -> Result<()> {
     let resp = ureq::get(&url).call().into_json_deserialize::<Response>()?;
     tx.send(resp.data.children)?;
     Ok(())
+}
+
+fn exists(url: String) -> Result<()> {
+    let resp = ureq::get(&url).call();
+    if resp.status() != 200 {
+        bail!("Not");
+    } else {
+        Ok(())
+    }
 }
 
 #[derive(Debug, Deserialize)]
